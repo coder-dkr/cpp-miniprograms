8ff4c5fa177ccea29d6c977bdab50dbfa3400d8f
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-15 18:56:03 +0530

    Variable declaration in cpp

---
+++ variables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//Variables declaration in c++
+
+int main(){
+    int num = 2;
+    char ch = 'w';
+    float fl = 34.5;
+    double largFl = 10.9999;
+
+    cout<<num<<endl<<ch<<endl<<fl<<endl<<largFl<<endl;
+
+    return 0;
+} 
