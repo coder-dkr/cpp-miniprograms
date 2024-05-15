eedc4255e537fc17a4244273bc4607b438b7a44b
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-15 20:39:04 +0530

    taking input in cpp

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    cout<<"the number u entered is : ["<<num<<"]"<<endl;
+
+    char ch;
+    cout<<"Enter a character: ";
+    cin>>ch;
+    cout<<"character u entered is : "<<ch;
+
+
+
+    return 0;
+}    
