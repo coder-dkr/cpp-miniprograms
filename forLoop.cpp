208693f481ec4e097ebdc45d8de0efae4e7de0a0
Dhruv Roy <dhruvakr9001@gmail.com>
2024-05-15 20:42:31 +0530

    For loop in cpp;

---
+++ forLoop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+//basic for lop
+
+int main(){
+    int num;
+    cout<<"Enter the number of times you want the puppy to woof: ";
+    cin>>num;
+
+    for(int i = 0; i< num; i++){
+        cout<<"woof ";
+    }
+
+
+    return 0;
+}
